f627e560cd0eaecbc07402d27b2cb2bb28797f08
Pavan Kumar <33474677+factopic@users.noreply.github.com>
2019-03-04 20:41:20 +0530

    Update SelectionSort.java

--- Sorting/SelectionSort/SelectionSort.java
+++ Sorting/SelectionSort/SelectionSort.java
@@ -8,10 +8,51 @@ public class SelectionSort
 		
 		for(int i=0; i<arr.length; i++)
 		{
+			int index = i;
 			for(int j=i+1; j<arr.length; j++)
 			{
+				if( arr[index] > arr[j] )
+				{
+					index = j;
+				}
+				
+				int small =  arr[index];
+				arr[index] = arr[i];
+				arr[i] = small;
 				
 			}
 		}
 	}
+	
+	
+	public static void main(String[] args)
+	{
+		int[] arr = {1,8,3,4,7,8,2,10,1};
+		
+		System.out.println( " Array Before Bubble Sorting ");
+
+        for(int i=0; i < arr.length; i++)
+        {  
+            System.out.print(arr[i] + " ");  
+        }  
+		
+		System.out.println();
+		
+		selectionSort(arr);
+		
+		System.out.println( " Array After Bubble Sorting ");
+		
+        for(int i=0; i < arr.length; i++)
+        {  
+            System.out.print(arr[i] + " ");  
+        }  
+		
+		
+		
+	}
+	
+	
+	
+	
+	
 }
