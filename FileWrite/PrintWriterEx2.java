4499dbcc698af847a84c81e9b11dcd3b333b92e1
Pavan Kumar <33474677+factopic@users.noreply.github.com>
2019-04-08 23:34:45 +0530

    p2

---
+++ FileWrite/PrintWriterEx2.java
@@ -0,0 +1,27 @@
+package FileWrite;
+
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class PrintWriterEx2 {
+	
+	public static void main(String[] args) throws Exception {
+		
+		 //Data to write on Console using PrintWriter  
+		PrintWriter pw = new PrintWriter(System.out);
+		String  str = "Hello Pavan"
+				+ "This message is from Java Program"
+				+ "How are you!"
+				+ "				Thank you....."
+				+ "Random"
+				;
+		pw.write(str);
+		pw.flush();  
+	    pw.close();
+	    
+	
+	    
+	}
+
+}
