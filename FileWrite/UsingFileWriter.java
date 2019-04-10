b169d1d7cf4e528f7376e50acc3455c1890aca29
Pavan Kumar <33474677+factopic@users.noreply.github.com>
2019-04-10 22:13:29 +0530

    Add files via upload

---
+++ FileWrite/UsingFileWriter.java
@@ -0,0 +1,21 @@
+package FileWrite;
+
+import java.io.FileWriter;
+
+
+public class UsingFileWriter {
+
+	public static void main(String[] args) throws Exception {
+		
+		FileWriter fw = new FileWriter("src\\FileWrite\\Output.txt" , true);
+		String  str = "Hello Pavan"
+				+ "This message is from Java Program"
+				+ "How are you!"
+				+ "				Thank you....."
+				+ "Random"
+				;
+						
+		fw.write(str);
+		fw.close();
+	}
+}
