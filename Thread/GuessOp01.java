d94e0a0570196ffad0ace1d5be5dd9dd8d225a5b
Pavan Kumar <33474677+factopic@users.noreply.github.com>
2019-04-18 16:22:29 +0530

    Add files via upload

---
+++ Thread/GuessOp01.java
@@ -0,0 +1,18 @@
+package Thread;
+
+public class GuessOp01 implements Runnable
+{
+	public void run()
+	{
+		System.out.printf("%d", 3);
+	}
+	
+	public static void main(String[] args) throws InterruptedException
+	{
+		Thread thread = new Thread(new GuessOp01() );
+		thread.start();
+		System.out.printf("%d",1);
+		thread.join(); 
+        System.out.printf("%d",2); 
+	}
+}
