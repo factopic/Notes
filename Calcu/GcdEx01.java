6b6212982672bcb9ecfa91566362955d8981539e
Pavan Kumar <33474677+factopic@users.noreply.github.com>
2019-05-13 23:32:02 +0530

    gcd with 2 method

--- Calcu/GcdEx01.java
+++ Calcu/GcdEx01.java
@@ -13,8 +13,32 @@ public class GcdEx01
 					b= b-a;             //System.out.println(a+" "+b);
 				
 			}
-			System.out.println(b);
+			System.out.println( "GCD with subtraction "+b);
+			
 		}
+		
+		
+		static void gcd1(int a , int b)
+		{
+			for ( int i=1; i<=a && 1<=b; i++ )                                   //java.lang.ArithmeticException: / by zero   if i=0
+			{
+				if( a%i ==0 && b%i==0 )
+				{
+					System.out.println( "GCD with reminder "+i);
+					System.out.printf("GCD of %d and %d is: %d", a, b, i);
+					System.out.println();
+				}
+				
+				
+			}
+		}
+		
+		
+		
+		
+		
+		
+		
 		
 		public static void main(String[] args)
 		{
@@ -23,6 +47,59 @@ public class GcdEx01
 			b = 121;
 			gcd( 121 , 55);
 			
-			gcd( 55 , 121);
+			gcd1( 55 , 121);
 		}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ * 
+ 
+  121-55=66
+  66-55=11
+  
+  55-11=44
+  44-11=33
+  33-11=22
+  22-11=11
+  Ans
+  
+ * 
+ * */
+
