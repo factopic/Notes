bd41c4610d66b05258b448f44f9214e58489f144
Pavan Kumar <33474677+factopic@users.noreply.github.com>
2019-05-22 22:28:16 +0530

    DayofWeek

---
+++ Date/DayDate.java
@@ -0,0 +1,26 @@
+package Date;
+
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class DayDate
+{
+	public static void main(String[] args) throws ParseException
+	{
+		String dd="05";
+		String MM="05";
+		String yyyy="1963";
+		
+		String inputDateStr = String.format("%s/%s/%s", dd, MM ,yyyy);
+		System.out.println( inputDateStr );
+		java.util.Date inputDate = new SimpleDateFormat("ddd/MM/yyyy").parse(inputDateStr);
+		Calendar calendar =  Calendar.getInstance();
+		calendar.setTime(inputDate);
+        String dayOfWeek = calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.US).toUpperCase();
+        System.out.println(dayOfWeek);
+	}
+
+}
