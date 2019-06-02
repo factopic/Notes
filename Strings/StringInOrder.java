6c2a681b03f42c07a98db77699959fe2c71793d0
Pavan Kumar <33474677+factopic@users.noreply.github.com>
2019-06-02 21:24:28 +0530

    Sort String

---
+++ Strings/StringInOrder.java
@@ -0,0 +1,18 @@
+package Strings;
+
+import java.util.Arrays;
+
+public class StringInOrder 
+{
+	public static void main(String[] args) 
+	{
+		
+	
+		
+		String[] st = {"Pavan", "Pradeep","quman","Chaitra"};
+		
+	      int size = st.length;
+	      Arrays.sort(st );
+	      System.out.println( Arrays.toString(st)  );
+	}
+}
